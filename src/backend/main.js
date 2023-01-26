4e9876f8631c1213af8f63056f7dd48076bb4914
M. A. Irsyad M. Aminuddin <contact@irsyadler.com>
2023-01-26 12:18:16 +0800

    Update backend assets

--- src/backend/main.js
+++ src/backend/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const { app, BrowserWindow, ipcMain } = require('electron');
-const nativeImage = require('electron').nativeImage;
+const { app, BrowserWindow, ipcMain, nativeImage } = require('electron');
 const path = require('path');
 const log = require('electron-log');
 
@@ -9,20 +8,20 @@ const data = require('./data');
 const frontend = { setStatus: undefined };
 
 const createWindow = () => {
-    // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 1280,
         height: 900,
         minWidth: 900,
         minHeight: 640,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
+            devTools: (app.commandLine.getSwitchValue("mode") === "dev") ? true : false, // Enable chrome dev tools if arg --mode=dev  
         },
         autoHideMenuBar: true,
         titleBarStyle: 'hidden',
         titleBarOverlay: {
             color: '#333',
-            symbolColor: '#00bcd4',
+            symbolColor: '#fff',
         },
 
         icon: nativeImage.createFromPath(path.join(path.dirname(__dirname), 'frontend/assets/images/icon-1024.png')),
@@ -37,12 +36,18 @@ const createWindow = () => {
     // Attach setStatus for the frontend
     frontend.setStatus = (text) => { mainWindow.webContents.send('update:status', text); };
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools();
+    // Enable development evironment if --mode=dev argument exist
+    if (app.commandLine.getSwitchValue("mode") === "dev") {
+        // Currently, below command unable to show the devtools window properly. Need to press Ctrl+Shift+I 3 times (while focusing on the main-window)
+        // mainWindow.webContents.openDevTools();
+    }
+    
 };
 
 app.whenReady().then(() => {
 
+    createWindow();
+    
     // Define ipc events
     ipcMain.on('log:info', (event, text) => { log.info(text); });
     ipcMain.on('log:error', (event, text) => { log.error(text); });
@@ -52,12 +57,12 @@ app.whenReady().then(() => {
     ipcMain.handle('data:checkDirectory', data.checkDirectory);
     ipcMain.handle('data:getAnnotation', (event, directoryData, config) => { return data.getAllPdfAnnotation(event, directoryData, config, frontend); });
     ipcMain.handle('action:openFile', data.openPdfFile);
-
     ipcMain.on('open:appUrl', (event) => {
         require('electron').shell.openExternal('https://github.com/irsyadler/NoteFinder');
     });
-
-    createWindow();
+    ipcMain.handle('editor:getInfo', data.getEditorInfo);
+    ipcMain.handle('editor:saveInfo', data.saveEditorInfo);
+    
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -71,6 +76,3 @@ app.on('window-all-closed', () => {
         app.quit();
     }
 });
-
-
-
