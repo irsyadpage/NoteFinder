cd8073ec3ced1d8125b57d2d6aeb16fa147bbc7c
M. A. Irsyad M. Aminuddin <contact@irsyad.page>
2023-05-04 14:58:56 +0800

    Update preload.js

--- src/backend/preload.js
+++ src/backend/preload.js
@@ -5,11 +5,12 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('backend', {
     log: (text) => ipcRenderer.send('log:info', text),
     logError: (text) => ipcRenderer.send('log:error', text),
-    getConfig: (reset) => ipcRenderer.invoke('config:get', reset),
+    getConfig: (reset) => ipcRenderer.invoke('config:get'),
     saveConfig: (configData) => ipcRenderer.invoke('config:save', configData),
+    appReset: () => ipcRenderer.invoke('app:reset'),
     chooseFile: (options) => ipcRenderer.invoke('action:chooseFile', options), // Async
     checkDirectory: (directoryList, recursive) => ipcRenderer.invoke('data:checkDirectory', directoryList, recursive), // Async
-    getAnnotation: (directoryData, config) => ipcRenderer.invoke('data:getAnnotation', directoryData, config), // Async
+    readDocument: (documentPath) => ipcRenderer.invoke('data:readDocument', documentPath), // Async
     openFile: (config, documentPath, documentPage) => ipcRenderer.invoke('action:openFile', config, documentPath, documentPage), 
     updateStatus: (callback) => ipcRenderer.on('update:status', callback),
     openAppUrl: () => ipcRenderer.send('open:appUrl'),
